200f524b047bc0ea860de4ee5488c5ca1069d0d1
farisan <muhammad.farisan99@gmail.com>
2022-11-03 19:24:51 +0700

    favorite sorting

--- src/repo/product.js
+++ src/repo/product.js
@@ -46,7 +46,7 @@ const search = (queryparams, hostAPI) => {
             link += `sorting=${queryparams.sorting}&`
         }
         if (queryparams.sorting == "favorite") {
-            query = "select product.*, transactions.qty from product left join transactions on transactions.product_id = product.id order by transactions.qty desc";
+            query = "select pr.*,p.code,p.valid,p.discount,COALESCE(sum(tr.qty),0) as sold from product pr left join promo p on pr.id = p.product_id left join transactions tr on pr.id = tr.product_id GROUP BY pr.id,p.code,p.valid,p.discount ORDER by sold desc";
             link += `sorting=${queryparams.sorting}&`
         }
 
