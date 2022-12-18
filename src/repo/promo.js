316fe18b79824b933e6c71fbf5c4071fd05a173e
farisan <muhammad.farisan99@gmail.com>
2022-12-18 10:09:23 +0700

    revisi image get all promo

--- src/repo/promo.js
+++ src/repo/promo.js
@@ -4,7 +4,7 @@ const postgreDb = require("../config/postgre")
 
 const get = () => {
     return new Promise((resolve, reject) => {
-        const query = "select promo.*, product.name, product.category, product.size, product.price, product.stock product.description from promo inner join product on product.id = promo.product_id";
+        const query = "select promo.*, product.name, product.category, product.size, product.price, product.stock from promo inner join product on product.id = promo.product_id";
         postgreDb.query(query, (err, result) => {
             if (err) {
                 console.log(err);
